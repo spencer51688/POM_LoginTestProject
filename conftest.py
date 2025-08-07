ac9d1c61927070b649c412bd9b78251ee7f18400
Spencer Wang <i8654028@gmail.com>
2025-08-07 21:14:06 +0800

    設定 Pytest 的共用 fixture
    設定 Pytest 的共用 fixture

--- conftest.py
+++ conftest.py
@@ -24,7 +24,7 @@ def driver(request):
     if request.node.rep_call.failed:
         allure.attach(driver.get_screenshot_as_png(), name="screenshot", attachment_type=allure.attachment_type.PNG)
     driver.quit()
-
+ 
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
     outcome = yield
