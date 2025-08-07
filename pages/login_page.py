25477e9768121905783869ce90197b1675d3cf30
Spencer Wang <i8654028@gmail.com>
2025-08-07 21:24:37 +0800

    建立登入頁與 Page Object 架構
    建立登入頁與 Page Object 架構

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+ 
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
